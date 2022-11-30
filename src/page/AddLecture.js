2209965029efa2ea0aacb6339df7dff00d81264d
Nahyun39 <nhhhh3999@gmail.com>
2022-11-30 13:23:32 +0900

    fix

---
+++ src/page/AddLecture.js
@@ -0,0 +1,249 @@
+// import React, {useState} from "react";
+
+// import {Button , Grid, TextField,Paper } from "@mui/material";
+/*
+class AddLecture extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { item: { title: "" } };
+        this.add = props.add;
+    }
+    onInputChange = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+        console.log(thisItem);
+    };
+    onButtonClick = () => {
+        this.add(this.state.item);
+        this.setState({ item: { title: "" } });
+    };
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            this.onButtonClick();
+        }
+    };
+    render() {
+        return (
+            <Paper style={{ margin: 16, padding: 16 }}>
+                <Grid container>
+                    <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                        <TextField
+                            placeholder="Add Lecture here"
+                            fullWidth
+                            onChange={this.onInputChange}
+                            value={this.state.item.title}
+                            onKeyPress={this.enterKeyEventHandler}
+                        />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button
+                            fullWidth
+                            color="secondary"
+                            variant="outlined"
+                            onClick={this.onButtonClick}
+                        >
+                            +
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        );
+    }
+}
+*/
+
+
+// const AddLecture = (props) =>{
+// //사용자의 입력을 저장할 오브젝트
+// //const [item , setItem] = useState({item: {lectureTitle:""}});
+//     const [state, setState] = useState({item: {lectureTitle:""}});
+//     const addItem = props.addItem;
+
+
+
+//     //onInputChange 함수
+//     const onInputChange = (e) =>{
+//         const thisItem = state.item;
+//         thisItem.lectureTitle=e.target.value;
+//         setState({item:thisItem});
+//         console.log(thisItem);
+//     };
+
+//     //onButtonClick 함수
+//     const onButtonClick =()=> {
+//         addItem(state.item);
+//         setState({item:{lectureTitle: ""}});//addItem 함수 사용
+//         //setItem({lectureTitle:""});
+//     }
+
+//     //enterKeyEvent 함수
+//     const enterKeyEventHandler =(e) =>{
+//         if(e.key === "Enter"){
+//             onButtonClick();
+//         }
+//     };
+
+//     //onInputChange 함수 ~ TextField 연결
+//     return(
+//         <Paper style = {{ margin: 16, padding: 16 }}>
+//             <Grid container>
+//                 <Grid xs ={11} md={11} item style={{paddingRight:16}}>
+//                     <TextField placeholder="Add Lecture here"
+//                                fullWidth
+//                                onChange={onInputChange}
+
+//                                value={state.item.lectureTitle}
+//                                onKeyPress={enterKeyEventHandler}
+//                     />
+//                 </Grid>
+//                 <Grid xs ={1} md={1} item>
+//                     <Button fullWidth style={{height:'100%'}} color ="secondary" variant="outlined"
+//                             onClick={onButtonClick}>
+//                         +
+//                     </Button>
+//                 </Grid>
+//             </Grid>
+//         </Paper>
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+// export default AddLecture;
+
+
+import React, {useState} from 'react';
+// import '../styles/AddmyStudents.css'
+import DatePicker from "react-datepicker";
+// import Header from './../components/Header';
+import axios from 'axios';
+import {useNavigate} from "react-router-dom";
+
+
+
+
+const AddmyStudents = () => {
+        const [color,setColor]=useState('');
+        console.log(color);
+
+        let navigate = useNavigate()
+
+        const [user, setUser] = useState({
+            lectureTitle:"",
+            stdName:"",
+            cycle:"",
+            fee:"",
+            minutesPerOnce:"",
+            dayOne:"",
+            dayTwo:"",
+            stdNumber:"",
+            momNumber:"",
+            schoolAge:""
+        })
+
+        const{lectureTitle, cycle, fee, minutesPerOnce, dayOne, dayTwo, stdNumber, momNumber, schoolAge, stdName} = user
+
+        const onInputChange=(e) =>{
+            setUser({...user,[e.target.name]:e.target.value})
+        }
+
+        const onSubmit= async(e)=>{
+            e.preventDefault();
+            
+            await axios.post("http://prod-eple-crystal-api-service.ap-northeast-2.elasticbeanstalk.com/eple/v1/mystudent/lecture", user)
+            console.log(e)
+            navigate("/mystudents")
+        }
+
+        return (
+            <form >
+                <div className='AddmyStudents'>
+                        {/* <Header>
+                            <h1>mystudents</h1>
+                        </Header> */}
+                        <form>
+                    <div className="container">
+                        <div className="lectureTitle">
+                            <label>과외 이름</label>
+                            <input name="lectureTitle" type="text" value={lectureTitle} onChange={(e)=>onInputChange(e)}/>
+                        </div>
+                        <div className="color">
+                            <div style={{background:'red',width:'50px',height:'50px'}} onClick={()=>setColor('red')}/>
+                            <div style={{background:'blue',width:'50px',height:'50px'}} onClick={()=>setColor('blue')}/>
+                            <div style={{background:'yellow',width:'50px',height:'50px'}} onClick={()=>setColor('yellow')}/>
+                            <div style={{background:'black',width:'50px',height:'50px'}} onClick={()=>setColor('#22222')}/>
+                            <div style={{background:'grey',width:'50px',height:'50px'}} onClick={()=>setColor('grey')}/>
+                            <input name={'color'} placeholder={color}/>
+                        </div>
+                        <div className="stdName">
+                            <label>학생 이름</label>
+                            <input name="stdName" type="text" value={stdName} onChange={(e)=>onInputChange(e)}/>
+                        </div>
+                        <div className="stdNumber">
+                            <label>학생 연락처</label>
+                            <input name="stdNumber" type="tel" value={stdNumber} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="momNumber">
+                            <label>부모님 연락처</label>
+                            <input name="momNumber" type="tel" value={momNumber} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="schoolAge">
+                            <label>학교/학년</label>
+                            <input name="schoolAge" value={schoolAge} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="">
+                            <label>학교/학년</label>
+                            <input name="schoolAge" value={schoolAge} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="fee">
+                            <label>과외 금액</label>
+                            <input name="fee" type="text" value={fee} onChange={(e)=>onInputChange(e)}/>만원
+                        </div>
+                        <div className="minutesPerOnce">
+                            <label>과외 진행 시간</label>
+                            <input name="minutesPerOnce" value={minutesPerOnce} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="dayOne">
+                            <label>과외 요일 및 시간</label>
+                            <input name="dayOne" value={dayOne} onChange={(e) => onInputChange(e)} />
+                            {/* <DatePicker
+                                className="dayOne"
+                                selected={dayOne}
+                                onChange={(e) => onInputChange(e)}
+                                timeInputLabel="Time:"
+                                dateFormat="MM/dd/yyyy h:mm aa"
+                                showTimeInput
+                            />                         */}
+                        </div>
+                        <div className="dayTwo">
+                            <label>과외 요일 및 시간</label>
+                            <input name="dayTwo" value={dayTwo} onChange={(e) => onInputChange(e)} />
+                            {/* <DatePicker
+                                className="dayTwo"
+                                selected={dayTwo}
+                                onChange={(e) => onInputChange(e)}
+                                timeInputLabel="Time:"
+                                dateFormat="MM/dd/yyyy h:mm aa"
+                                showTimeInput
+                            />     */}
+                        </div>
+
+                        <div className="btn">
+                            <button type="submit"  onSubmit={e => onSubmit(e)}>작성완료</button>
+                        </div>
+                        </div>
+                        </form>
+                </div>
+            </form>
+    );
+}
+
+export default AddmyStudents;
--- src/page/MyStudent.js
+++ src/page/MyStudent.js
@@ -1,93 +1,224 @@
-import React, { useState, useEffect } from "react";
-import "../App.css";
-import Lecture from "../components/Lecture";
-import {Container, List, Paper} from "@mui/material";
-import AddLecture from "../components/AddLecture.js"
-import {call,signout} from "../service/ApiService";
+// import React, { useState, useEffect } from "react";
+// import "../App.css";
+// import Lecture from "../components/Lecture";
+// import {Container, List, Paper} from "@mui/material";
+// import AddLecture from "./AddLecture"
+// import {call,signout} from "../service/ApiService";
 
 
 
-const MyStudent = () => {
-    const [state, setState] = useState({ items: []});
-    const [loading, setLoading] = useState(true);
+// const MyStudent = () => {
+//     const [state, setState] = useState({ items: []});
+//     const [loading, setLoading] = useState(true);
 
 
-    // componentDidMount 대신 userEffect 사용
-    useEffect(() => {
-        call("/eple/v1/mystudent/lecture", "GET", null).then((response) =>{
-            setState({items: response.data});
+//     // componentDidMount 대신 userEffect 사용
+//     useEffect(() => {
+//         call("/eple/v1/mystudent/lecture", "GET", null).then((response) =>{
+//             setState({items: response.data});
 
-            setLoading(false);
-        });
-    }, []);
+//             setLoading(false);
+//         });
+//     }, []);
 
-    const addItem = (item) => {
+//     const addItem = (item) => {
 
-        const thisItems = state.items;
-        item.id = "ID-" + thisItems.length; // key를 위한 id추가
-        thisItems.push(item); // 배열에 아이템 추가
-        setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
-        console.log("items : ", state.items);
-
-
-
-
-        call("/eple/v1/mystudent/lecture", "POST", item).then((response) =>
-            setState({ items: response.data })
-        );
-    };
-
-    const deleteItem = (item) => {
-        call("/eple/v1/mystudent/lecture", "DELETE", item).then((response) =>
-            setState({ items: response.data })
-        );
-    };
-
-    const updateItem = (item) => {
-        call("/eple/v1/mystudent/lecture", "PUT", item).then((response) =>
-            setState({ items: response.data })
-        );
-    };
+//         const thisItems = state.items;
+//         item.id = "ID-" + thisItems.length; // key를 위한 id추가
+//         thisItems.push(item); // 배열에 아이템 추가
+//         setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
+//         console.log("items : ", state.items);
 
 
 
-    var lectureItems = state.items.length > 0 && (
-        <Paper style={{ margin: 16 }}>
-            <List>
-                {state.items.map((item, idx) => (
-                    <Lecture
-                        item={item}
-                        key={item.id}
-                        deleteItem={deleteItem}
-                        updateItem={updateItem}
-                    />
-                ))}
-            </List>
-        </Paper>
-    );
 
-    //로딩중이 아닐 때 렌더링할 부분
-    let lectureListPage = (
-        <div>
-            <Container maxWidth="md">
-                <AddLecture addItem={addItem} />
-                <div className="LectureList">{lectureItems}</div>
-            </Container>
-        </div>
-    );
+//         call("/eple/v1/mystudent/lecture", "POST", item).then((response) =>
+//             setState({ items: response.data })
+//         );
+//     };
 
-    let loadingPage = <h1> 로딩중..</h1>;
-    let content = loadingPage;
-    if(!loading){
-        //로딩중이 아니면 lecture page
-        content = lectureListPage;
+//     const deleteItem = (item) => {
+//         call("/eple/v1/mystudent/lecture", "DELETE", item).then((response) =>
+//             setState({ items: response.data })
+//         );
+//     };
+
+//     const updateItem = (item) => {
+//         call("/eple/v1/mystudent/lecture", "PUT", item).then((response) =>
+//             setState({ items: response.data })
+//         );
+//     };
+
+
+
+//     var lectureItems = state.items.length > 0 && (
+//         <Paper style={{ margin: 16 }}>
+//             <List>
+//                 {state.items.map((item, idx) => (
+//                     <Lecture
+//                         item={item}
+//                         key={item.id}
+//                         deleteItem={deleteItem}
+//                         updateItem={updateItem}
+//                     />
+//                 ))}
+//             </List>
+//         </Paper>
+//     );
+
+//     //로딩중이 아닐 때 렌더링할 부분
+//     let lectureListPage = (
+//         <div>
+//             <Container maxWidth="md">
+//                 <AddLecture addItem={addItem} />
+//                 <div className="LectureList">{lectureItems}</div>
+//             </Container>
+//         </div>
+//     );
+
+//     let loadingPage = <h1> 로딩중..</h1>;
+//     let content = loadingPage;
+//     if(!loading){
+//         //로딩중이 아니면 lecture page
+//         content = lectureListPage;
+//     }
+
+//     // 3. props로 넘겨주기
+//     return <div className="MyStudent"> {content}
+//     </div>
+
+// };
+
+// export default MyStudent;
+
+import React, {Fragment} from 'react';
+// import Mystudents from './../components/Mystudents'
+import '../styles/Mystudents.css'
+// import Header from './../components/Header';
+import { Link } from "react-router-dom";
+// import axios from '../utils.js'
+import axios from 'axios';
+import {useEffect, useState} from "react";
+import MyStudent from './MyStudent';
+
+
+const studentsData = [
+    {
+            "lecture_Code": 1,
+            "lectureTitle": "영어독해",
+            "color": null,
+            "stdName": '김철수',
+            "cycle": '3회/8회',
+            "fee": 100000,
+            "minutesPerOnce": 0,
+            "day_1": '월',
+            "day_2": '화',
+            "stdNumber": null,
+            "momNumber": null,
+            "schoolAge": null,
+            "time_1": null,
+            "time_2": null,
+            "scheduleCounter": 0,
+            "presentCycle": 0,
+            "scheduleEntities": null
+    },
+    {
+        "lecture_Code": 2,
+        "lectureTitle": "고등 상 수학",
+        "color": null,
+        "stdName": '김하림',
+        "cycle": '2회/10회',
+        "fee": 200000,
+        "minutesPerOnce": 0,
+        "day_1": '수',
+        "day_2": '금',
+        "stdNumber": null,
+        "momNumber": null,
+        "schoolAge": null,
+        "time_1": null,
+        "time_2": null,
+        "scheduleCounter": 0,
+        "presentCycle": 0,
+        "scheduleEntities": null
+    },
+    {
+        "lecture_Code": 3,
+        "lectureTitle": "영어 문법",
+        "color": null,
+        "stdName": '김선생',
+        "cycle": '3회/8회',
+        "fee": 300000,
+        "minutesPerOnce": 0,
+        "day_1": '금',
+        "day_2": '토',
+        "stdNumber": null,
+        "momNumber": null,
+        "schoolAge": null,
+        "time_1": null,
+        "time_2": null,
+        "scheduleCounter": 0,
+        "presentCycle": 0,
+        "scheduleEntities": null
     }
+]
 
-    // 3. props로 넘겨주기
-    return <div className="MyStudent"> {content}
-    </div>
+const Mystudents = () => {
+    const [users, setUsers] = useState([]);
+    const [result, setResult] = useState([]);
 
-};
+    useEffect(()=>{
+        loadUser();
+        //ssetUsers(studentsData);
+    },[]);
 
-export default MyStudent;
+    const loadUser =  async() => {
+        const result = await axios.get("http://prod-eple-crystal-api-service.ap-northeast-2.elasticbeanstalk.com/eple/v1/mystudent/lecture");
+        console.log(result,"asd");
+        setUsers(result.data);
+        //const result = await axios.get('https://reqres.in/api/users?page=1')
+        //setResult(result.data.data)
+    }
+    return(
+        <div className="container">s
+            <h1>학생 관리 페이지</h1>
+            <Link to = '/AddLecture'>
+                <button>학생 추가하기</button>
+            </Link>
+            <div className="card">
+                <div className="card-title">
+                    
+                </div>
+                <div className="card-body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>이름</td>
+                                <td>과목</td>
+                                <td>진행회차</td>
+                                <td>수업진행요일</td>
+                                <td>과외 금액</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((data) =>{
+                                    <tr>
+                                        <th scope='row'>{data.stdName}</th>
+                                        <div className="col">{data.lectureTitle}</div>
+                                        <div className="col">{data.cycle}</div>
+                                        {<div className="col">{`${data.day_1}${data.day_2}`}</div>}
+                                        {<div className="col">{data.fee.toLocaleString()}만원</div>}
+                                    </tr>
+                                })
 
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Mystudents;
