027d20c3a2292ec69ccfc71cbe6300e03e34363b
Jeongsu Lee <107098751+JeongSu0880@users.noreply.github.com>
2022-12-01 00:05:23 +0900

    Revert "FIX"
    This reverts commit 52df79c13654619744beafd03d7a292906b827be.

--- src/page/Calendar.js
+++ src/page/Calendar.js
@@ -29,18 +29,16 @@ export default function Calendar() {
         headers.append("Authorization","Bearer "+accessToken);
     }
 
-    const [state, setState] = useState({ eventlist: [
-        
-    ]});
+    const [state, setState] = useState({ eventlist: []});
     const calendarRef = useRef(null);
     const [modalOpen, setModalOpen ] = useState(false); 
     const [isLecture, setIsLecture] = useState(false);
 
     const [addedevent, setAddedevent] = useState({
-        id: "",
+        //id: "",
         lectureTitle: "",
-        start:  moment().format("yyyy-MM-dd HH:mm"),
-        end:  moment().format("yyyy-MM-dd HH:mm")
+        start: "",
+        end: ""
     });
 
     // useEffect(() => {
@@ -119,11 +117,10 @@ export default function Calendar() {
     }
     
     function handleDataName(event){
-        const thisEvents = state.eventlist;
         addedevent.lectureTitle = JSON.stringify(event.title);
         addedevent.start = moment(event.start).format("yyyy-MM-dd HH:mm");
         addedevent.end = moment(event.end).format("yyyy-MM-dd HH:mm");
-        addedevent.id = "ID-" + thisEvents.length;
+        //addedevent.id = "ID-" + thisEvents.length;
         
         return addedevent;
     }
