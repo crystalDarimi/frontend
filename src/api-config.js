3f4541b89ba39c6c41e7981725f2d65582f29e43
Jeongsu Lee <107098751+JeongSu0880@users.noreply.github.com>
2022-12-06 19:48:03 +0900

    FIX

--- src/api-config.js
+++ src/api-config.js
@@ -3,7 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if(hostname ==="localhost"){
-    backendHost = "http://prod-eple-crystal-api-service.ap-northeast-2.elasticbeanstalk.com"
+    backendHost = "http://eple-v2-api-service.ap-northeast-2.elasticbeanstalk.com/"
 }
 
 export const API_BASE_URL = `${backendHost}`;
