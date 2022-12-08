f235f92d74524cc63c68d27cebb9f6c64ab55132
cotton-ball <nnnnn9489@naver.com>
2022-12-08 23:09:19 +0900

    [feat] temp

--- src/api-config.js
+++ src/api-config.js
@@ -3,7 +3,9 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if(hostname ==="localhost"){
-    backendHost = "http://eple-api-service.ap-northeast-2.elasticbeanstalk.com"
+    backendHost = "http://localhost:8080";
+}else {
+    backendHost = "http://eple-api-service.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
