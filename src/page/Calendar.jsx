542cea73d30ed4808eafe1901ab815ab3397d974
Jeongsu Lee <107098751+JeongSu0880@users.noreply.github.com>
2022-12-08 10:05:01 +0900

    ADD : 자잘한 수정

--- src/page/Calendar.jsx
+++ src/page/Calendar.jsx
@@ -63,12 +63,12 @@ export default function Calendar(user) {
 
         //첫번째는 fullcalendar에 들어가는 lectureTitle을 title로 변경한 것입니다.
         //const thisEvents = state.eventlist;
-        addedCalendar.title = added.lectureTitle.value;
+        addedCalendar.title = added.lectureTitle;
         addedCalendar.start = added.start;
         addedCalendar.end = added.end;
 
         //두번째는 post시에 들어갈 날짜는 백엔드에서 설정한 포멧으로 맞추는 코드입니다.
-        addedData.lectureTitle = added.lectureTitle.value;
+        addedData.lectureTitle = added.lectureTitle;
         addedData.start = moment(added.start).format("yyyy-MM-dd HH:mm");
         addedData.end = moment(added.end).format("yyyy-MM-dd HH:mm");
 
@@ -140,7 +140,7 @@ export default function Calendar(user) {
                     </div> 
                 </button>
                 {hoverBtn && <div className='hoverButtons'>
-                    {/*<button className='schedule'onMouseEnter={openMouseHover} onMouseLeave= {closeMouseHover} onClick={()=> {setModalOpen(true); setIsLecture(false)}}>일정 추가</button>*/}
+                    <button className='schedule'onMouseEnter={openMouseHover} onMouseLeave= {closeMouseHover} onClick={()=> {setModalOpen(true); setIsLecture(false)}}>일정 추가</button>
                     <button className='lecture'onMouseEnter={openMouseHover} onMouseLeave={closeMouseHover} onClick={()=> {setModalOpen(true); setIsLecture(true)}}>과외 추가</button>
                     </div>}
                 </div>
